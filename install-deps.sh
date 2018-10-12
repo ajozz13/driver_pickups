8df23766650c3783e32cdcc7ce2f1614b81de55e
aochoa <alberto.ochoa@ibcinc.com>
2018-10-12 08:28:14 -0400

    script to documents and install dependencies

---
+++ install-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+npm install express
+npm install mongoose
+npm install winston
+
+npm install --save-dev jasmine
+npm install --save-dev jasmine-spec-reporter
+npm install --save-dev request

2e695014e826915ae2ae356e3aa341aaf3f626bc
aochoa <alberto.ochoa@ibcinc.com>
2018-11-02 11:26:11 -0400

    correct names for the resources and also add the static resource

--- api/routes/pickupsRoutes.js
+++ api/routes/pickupsRoutes.js
@@ -1,24 +1,24 @@
 'use strict';
 
-module.exports = function( app ){
+module.exports = function( app, express ){
   var manager = require( '../controllers/pickupsController' ),
     path = require( 'path' ),
     config = require( '../../config' );
 
-  app.route( config.pickups_manager_resource )
-    .get( function( req, res ){
-      res.sendFile( path.join( __dirname + '../../../static/manager/index.html' ) )
-    });
-
   app.route( config.pickups_resource )
+      .get( manager.list_all_pickups )
       .post( manager.create_pickup );
 
   app.route( config.pickups_resource+ '/:station' )
-      .get( manager.list_all_pickups );
+      .get( manager.list_pickups_by_station );
 
   app.route( config.pickups_resource+ '/:station/:resource_id' )
       .get( manager.list_pickup )
       .post( manager.update_entry )
       .options( manager.options_handler )
       .delete( manager.remove_entry );
+
+  //Static routes
+  app.use( config.pickups_manager_resource, express.static( 'static/manager' ) );
+
 }
